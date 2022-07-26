b1ae7d10f70a76c16c58ed9351fe92e0dd09354e
D.H.LEE <108387377+PRO-DH@users.noreply.github.com>
2022-07-26 09:57:13 +0900

    wrapper2

---
+++ 2. Java/practice56.java
@@ -0,0 +1,15 @@
+package wrapper;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		
+		String number = "100a";
+		
+		int myNumber = Integer.parseInt(number);
+
+		System.out.println(myNumber+200);
+		
+	}
+
+}
