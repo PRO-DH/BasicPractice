d80b33ecc3e485e671d439d2f00754d219a654dc
D.H.LEE <108387377+PRO-DH@users.noreply.github.com>
2022-08-29 19:11:44 +0900

    Create P10872.java

---
+++ 4. BaekJoon/P10872.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		in.close();
+		
+		int sum = 1;
+        
+		while(N != 0) {
+			sum = sum * N;
+			N--;
+		}
+        
+		System.out.println(sum);
+		
+	}
+}
