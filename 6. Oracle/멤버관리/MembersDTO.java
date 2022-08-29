79df0457f0d7dfa89f3fbdcd49f1d03a1fccdb0f
D.H.LEE <108387377+PRO-DH@users.noreply.github.com>
2022-08-29 10:34:39 +0900

    Create MembersDTO.java

---
+++ 6. Oracle/멤버관리/MembersDTO.java
@@ -0,0 +1,76 @@
+package com.jsp.members;
+
+import java.sql.Timestamp;
+
+public class MembersDTO {
+	
+	private String id;
+	private String pw;
+	private String name;
+	private String email;
+	private String address;
+	private Timestamp regDate;
+	
+	public MembersDTO() {}
+
+	public MembersDTO(String id, String pw, String name, String email, Timestamp regDate, String address) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.regDate = regDate;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Timestamp regDate) {
+		this.regDate = regDate;
+	}
+	
+	
+
+}
